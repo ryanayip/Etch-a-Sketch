ccd786480966aa8115fbb454db9c562926782b98
Ryan Yip <ryip94@yahoo.com>
2022-07-26 05:43:50 -0700

    Added reset button to clear page

--- js.js
+++ js.js
@@ -5,13 +5,17 @@ function makeRows(rows, cols) {
   container.style.setProperty('--grid-cols', cols);
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
-    cell.innerText = (c + 1);
+    //cell.style.backgroundColor = "white";
     cell.addEventListener("mouseover", changeColor);
+    cell.style.backgroundColor = "white"
     container.appendChild(cell).className = "gridItem";
+    
+    
   };
+  
 };
 
-makeRows(16, 16);
+makeRows(100, 100);
 
 
 
@@ -22,6 +26,34 @@ function changeColor() {
     this.style.backgroundColor = "black";
 }
 
+function changeSize() {    
+    
+    let x = prompt("How many squares wide? Choose between 1-100.");
 
+    if (isNaN(x)) {
+        alert("That's not a number...");
+        return;
+    }
 
+    if (x > 100 || x < 1) {
+        alert("You'll crash your device with that many squares! How about between 1 and 100?")
+    } else {
+    makeRows(x, x);
+    gridItems.forEach((gridItem) => (gridItem.style.backgroundColor = "white"));
 
+    
+    
+    
+    }}
+
+const onClickBtn = document.querySelector("#addSquares");
+onClickBtn.onclick = changeSize
+
+const resetBtn = document.querySelector("#resetBoard")
+resetBtn.onclick = reset
+
+function reset() {
+    gridItems.forEach((gridItem) => (gridItem.style.backgroundColor = "white"));
+    makeRows(100, 100);
+    gridItems.forEach((gridItem) => (gridItem.style.backgroundColor = "white"));
+  }
